764e00b44c34287708b95872a3688f9b1d42a91e
Basitan Schülke <bas.schuelke@googlemail.com>
2023-02-16 16:12:09 +0100

    Fixed psql to msql

--- src/modals/save_token.ts
+++ src/modals/save_token.ts
@@ -34,7 +34,7 @@ export default class extends Modal {
         }
         if(!user_data) return ctx.error({error: "Unable to find user with this token!"})
         const token = ctx.client.config.advanced?.encrypt_token ? ctx.client.encryptString(raw_token) : raw_token
-        const res = await ctx.database.query("INSERT INTO user_tokens VALUES (DEFAULT, $1, $2) ON CONFLICT (id) DO UPDATE SET token=$2 RETURNING *", [ctx.interaction.user.id, token])
+        const res = await ctx.database.query("INSERT INTO user_tokens VALUES (DEFAULT, $1, $2) ON DUPLICATE KEY UPDATE token=$2", [ctx.interaction.user.id, token])
         if(res.affectedRows < 1) return ctx.error({error: "Unable to save token"})
         await ctx.interaction.reply({
             content: `S${ctx.client.config.advanced?.encrypt_token ? "ecurely s" : ""}aved your token in the database.`,
